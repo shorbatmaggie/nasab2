7686c77304ce92de641b8895f2b3db515783e231
shorbatmaggie <maggies@nyu.edu>
2025-06-19 01:43:23 -0400

    Update ff3a18430120a410@676.js

--- ff3a18430120a410@676.js
+++ ff3a18430120a410@676.js
@@ -166,7 +166,7 @@ function _dropdown(fullData) {
   resetBtn.className = "mt-2 px-4 py-2 rounded shadow";
   resetBtn.style.backgroundColor = "#588B8B";
   resetBtn.style.color = "white";
-  
+
   resetBtn.onclick = () => {
     window.setFilteredData(fullData);
 
@@ -2373,7 +2373,7 @@ function _constructTangleLayout(d3){return(
   const metro_d = 4;
   const min_family_height = 22;
   const generationSpacing = 250;
-  const bundleClearance = 300;
+  const bundleClearance = 200;
   const labelPadding = 500; // enough for long Arabic/English titles
   const baseGenerationSpacing = 250;
   const minContentWidth = 1280;
