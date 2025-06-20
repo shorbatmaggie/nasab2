d99b562d63c7631b6125175f45c960c093e2748b
shorbatmaggie <maggies@nyu.edu>
2025-06-19 23:42:27 -0400

    Update ff3a18430120a410@676.js

--- ff3a18430120a410@676.js
+++ ff3a18430120a410@676.js
@@ -379,10 +379,13 @@ function _renderChart(color, constructTangleLayout, _, svg, background_color, d3
 
       const resetBtn = document.createElement("button");
       resetBtn.innerHTML = `
-        <svg width="18" height="18" viewBox="0 0 18 18" style="display:block;margin:auto;" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <circle cx="9" cy="9" r="9" fill="#588B8B"/>
-          <path d="M6.5 11C8 9 12 9 12 9V7l-3 3 3 3v-2s-4 0-5.5 2" stroke="#fff" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round" fill="none"/>
-        </svg>`;
+      <svg width="18" height="18" viewBox="0 0 18 18" fill="none" style="display:block;margin:auto;" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="9" cy="9" r="9" fill="#588B8B"/>
+        <path d="M12.5 12c-1.2 1.2-4.3 1.1-5.2-1.1M7.3 10.9L6 12.2M7.3 10.9V13M7.3 10.9h2.1" stroke="#fff" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M13 6A5 5 0 0 0 6 5.7" stroke="#fff" stroke-width="1.5" fill="none" stroke-linecap="round"/>
+      </svg>
+    `;
+
       resetBtn.style = buttonStyle;
       resetBtn.title = "Reset zoom";
 
