5f33642aac3737d59d42222821a8cdf642171861
shorbatmaggie <maggies@nyu.edu>
2025-06-21 15:10:02 -0400

    Update ffdefault_full_tree.js

--- ffdefault_full_tree.js
+++ ffdefault_full_tree.js
@@ -317,7 +317,7 @@ function _constructTangleLayout(d3){return(
   const metro_d = 4;
   const min_family_height = 22;
   const generationSpacing = 250;
-  const bundleClearance = 400;
+  const bundleClearance = 450;
   const labelPadding = 800; // enough for long Arabic/English titles
   const baseGenerationSpacing = 450;
   const minContentWidth = 1280;
